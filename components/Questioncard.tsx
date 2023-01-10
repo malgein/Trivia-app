58befb34c36fd4adc5a9632489c314032c786473
Wilmer Pocaterra <malgein17@gmail.com>
2023-01-10 11:31:09 -0400

    Initializing the project

---
+++ components/Questioncard.tsx
@@ -0,0 +1,36 @@
+import styles from '../styles/QuestionCard.module.css'
+
+type Props = {
+	question: string;
+	answers: string[];
+	callback: any;
+	userAnswer: any;
+	questionNr: number;
+	totalQuestions: number;
+}
+
+const QuestionCard: React.FC<Props> = ({
+	question, 
+	answers, 
+	callback, 
+	userAnswer, 
+	questionNr, 
+	totalQuestions}) => {
+	return(
+		<div>
+			<p className={styles.number}>Question: {questionNr} / {totalQuestions}</p>
+			<p dangerouslySetInnerHTML={{ __html: question }} />
+			<div>
+				{answers.map(answer => (
+					<div key={answer}>
+						<button disabled={userAnswer} onClick={callback}>
+							<span dangerouslySetInnerHTML={{ __html:answer}} />
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default QuestionCard
